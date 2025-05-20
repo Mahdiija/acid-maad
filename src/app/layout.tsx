92f61e0ad8f0b62e31ada323acba6060bcc7909c
mahdi jalinousi <141873601+Mahdiija@users.noreply.github.com>
2025-05-20 12:24:08 +0330

    fix: correct title in metadata
    The title in the metadata was corrected from "FilmFlix" to "Film" to accurately reflect the project name.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
-  title: "FilmFlix - Watch Movies Online",
+  title: "Film- Watch Movies Online",
   description: "Watch the latest movies online in HD quality",
 };
 
